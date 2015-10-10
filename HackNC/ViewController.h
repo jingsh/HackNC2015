58a59ce6e03b75574f97c3f4310f639b886d9073
Jing <shanjing.titan@gmail.com>
2015-10-10 10:31:26 -0400

    Initial Commit

---
+++ HackNC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackNC
+//
+//  Created by Jing Shan on 10/10/15.
+//  Copyright © 2015 Jing Shan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HackNC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HackNC
+//
+//  Created by Jing Shan on 10/10/15.
+//  Copyright © 2015 Jing Shan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
